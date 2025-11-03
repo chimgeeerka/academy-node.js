8628149a3bfe609d3df222247f27128fc7f07e23
BACK-0206 <back-0206@Baterdenes-Mac-mini.local>
2025-11-03 21:54:28 +0800

    main

--- bank.js
+++ bank.js
@@ -1,11 +1,9 @@
-// BankAccount.js
 class BankAccount {
   constructor(owner, balance) {
     this.owner = owner;
     this.balance = balance;
   }
 
-  // Add money
   deposit(amount) {
     if (amount <= 0) {
       console.log("Deposit amount must be positive!");
@@ -14,7 +12,6 @@ class BankAccount {
     this.balance += amount;
   }
 
-  // Withdraw money
   withdraw(amount) {
     if (amount > this.balance) {
       console.log("Not enough balance!");
@@ -23,14 +20,13 @@ class BankAccount {
     this.balance -= amount;
   }
 
-  // Get current balance
   getBalance() {
     return this.balance;
   }
 }
 
-// Example usage
+
 const account = new BankAccount("Enkhee", 100);
-account.deposit(50);    // +50 → 150
-account.withdraw(30);   // -30 → 120
-console.log(account.getBalance()); // 120
+account.deposit(50);  
+account.withdraw(30);   
+console.log(account.getBalance()); 
--- index.js
+++ index.js
@@ -23,3 +23,6 @@ try {
 process.exit();
 
 
+
+
+
