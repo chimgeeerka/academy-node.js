3b2b3a4fc03f106d82f4a20e730d7d1274fc8d9f
BACK-0206 <back-0206@Baterdenes-Mac-mini.local>
2025-11-18 20:17:03 +0800

    style.css

--- routers/bank.js
+++ routers/bank.js
@@ -1,203 +1,198 @@
-import express from "express";
-import { checkBalance } from "../controller/bank.js";
+// import express from "express";
+// import { checkBalance } from "../controller/bank.js";
 
-export const bankRouter = new express.Router();
+// export const bankRouter = new express.Router();
 
-bankRouter.get("/check-balance", checkBalance);
+// bankRouter.get("/check-balance", checkBalance);
 
-bankRouter.get("/get-history", ( req, res ) => {
-    res.send(3000);
-});
-
-
+// bankRouter.get("/get-history", ( req, res ) => {
+//     res.send(3000);
+// });
 
 
 
+import inquirer from "inquirer";
+import fs from "node:fs/promises";
+
+export const bankAnswer = async (users, user) => {
+  const { bankOption } = await inquirer.prompt([
+    {
+      type: "select",
+      name: "bankOption",
+      message: "Login Or Signup",
+      choices: [
+        { name: "Deposit", value: "deposit" },
+        { name: "Withdraw", value: "withdraw" },
+        { name: "History all", value: "history-all" },
+        { name: "History deposit", value: "history-deposit" },
+        { name: "History withdraw", value: "history-withdraw" },
+        { name: "Check balance", value: "check-balance" },
+        { name: "Transaction", value: "transaction" },
+        { name: "Exit", value: "exit" }
+      ]
+    }
+  ]);
 
 
 
-// import inquirer from "inquirer";
-// import fs from "node:fs/promises";
+  switch (bankOption) {
+    case "deposit":
+      await deposit(users, user);
+      break;
+    case "withdraw":
+        await withdraw (users,user)
+      console.log("withdraw");
+      break;
+    case "history-all":
+        await historyAll(user)
+      console.log("history");
+      break;
+    case "check-balance":
+      console.log("check-balance");
+      break;
+    case "exit":
+      process.exit();
+  }
+};
 
-// export const bankAnswer = async (users, user) => {
-//   const { bankOption } = await inquirer.prompt([
-//     {
-//       type: "select",
-//       name: "bankOption",
-//       message: "Login Or Signup",
-//       choices: [
-//         { name: "Deposit", value: "deposit" },
-//         { name: "Withdraw", value: "withdraw" },
-//         { name: "History all", value: "history-all" },
-//         { name: "History deposit", value: "history-deposit" },
-//         { name: "History withdraw", value: "history-withdraw" },
-//         { name: "Check balance", value: "check-balance" },
-//         { name: "Transaction", value: "transaction" },
-//         { name: "Exit", value: "exit" }
-//       ]
-//     }
-//   ]);
+const updateUser = async (users, user, amount, type) => {
+  const userData = JSON.stringify(users);
 
+  await fs.writeFile("users.json", userData, "utf-8");
 
+  const historyRawData = await fs.readFile("history.json", "utf-8");
+  const history = JSON.parse(historyRawData);
 
-//   switch (bankOption) {
-//     case "deposit":
-//       await deposit(users, user);
-//       break;
-//     case "withdraw":
-//         await withdraw (users,user)
-//       console.log("withdraw");
-//       break;
-//     case "history-all":
-//         await historyAll(user)
-//       console.log("history");
-//       break;
-//     case "check-balance":
-//       console.log("check-balance");
-//       break;
-//     case "exit":
-//       process.exit();
-//   }
-// };
+  const userHistories = history[user.username] || [];
 
-// const updateUser = async (users, user, amount, type) => {
-//   const userData = JSON.stringify(users);
+  userHistories.push({
+    type,
+    amount,
+    balance: user.balance,
+    currentBalance: 0
+  });
 
-//   await fs.writeFile("users.json", userData, "utf-8");
+  history[user.username] = userHistories;
 
-//   const historyRawData = await fs.readFile("history.json", "utf-8");
-//   const history = JSON.parse(historyRawData);
+  const historyData = JSON.stringify(history);
 
-//   const userHistories = history[user.username] || [];
+  await fs.writeFile("history.json", historyData, "utf-8");
+  console.log("Amjilttai");
 
-//   userHistories.push({
-//     type,
-//     amount,
-//     balance: user.balance,
-//     currentBalance: 0
-//   });
+  return;
+};
 
-//   history[user.username] = userHistories;
+const deposit = async (users, user) => {
+  let balance = parseInt(user.balance) || 0;
 
-//   const historyData = JSON.stringify(history);
+  const { amount } = await inquirer.prompt([
+    {
+      type: "number",
+      name: "amount",
+      message: "Hediin orlogo hiih we?"
+    }
+  ]);
 
-//   await fs.writeFile("history.json", historyData, "utf-8");
-//   console.log("Amjilttai");
+  balance = balance + amount;
 
-//   return;
-// };
+  user.balance = balance;
 
-// const deposit = async (users, user) => {
-//   let balance = parseInt(user.balance) || 0;
-
-//   const { amount } = await inquirer.prompt([
-//     {
-//       type: "number",
-//       name: "amount",
-//       message: "Hediin orlogo hiih we?"
-//     }
-//   ]);
-
-//   balance = balance + amount;
-
-//   user.balance = balance;
-
-//   return await updateUser(users, user, amount, "deposit");
-// };
+  return await updateUser(users, user, amount, "deposit");
+};
 
 
-// const withdraw = async (users, user) => {
-//     let balance = parseInt(user.balance) || 0;
+const withdraw = async (users, user) => {
+    let balance = parseInt(user.balance) || 0;
 
-//    const { amount } = await inquirer.prompt([
-//     {
-//       type: "number",
-//       name: "amount",
-//       message: "Hediin zarlaga hiih we?"
-//     }
-//   ]);
+   const { amount } = await inquirer.prompt([
+    {
+      type: "number",
+      name: "amount",
+      message: "Hediin zarlaga hiih we?"
+    }
+  ]);
 
-//   console.log(balance,'balance')
-//   console.log(amount,"amount")
-//   if(balance > amount){
-//     console.log(
-//         "1"
-//     )
-//   balance = balance - amount;
+  console.log(balance,'balance')
+  console.log(amount,"amount")
+  if(balance > amount){
+    console.log(
+        "1"
+    )
+  balance = balance - amount;
 
-//   user.balance = balance;
+  user.balance = balance;
 
-//   return await updateUser(users, user, amount, "deposit");
-//   }else{
-//     console.log("amount aa shalgana uu" + balance)
-//   }
+  return await updateUser(users, user, amount, "deposit");
+  }else{
+    console.log("amount aa shalgana uu" + balance)
+  }
     
-// };
+};
 
 
 
 
 
-// const historyAll = async ( user ) => {
+const historyAll = async ( user ) => {
 
 
-//   const historyRawData = await fs.readFile("history.json", "utf-8");
-//   const history = JSON.parse(historyRawData);
+  const historyRawData = await fs.readFile("history.json", "utf-8");
+  const history = JSON.parse(historyRawData);
 
-//   const userHistories =[
-//   { type: 'deposit', amount: 2000, balance: 6000, currentBalance: 0 },
-//   {
-//     type: 'deposit',
-//     amount: 100000,
-//     balance: 100000,
-//     currentBalance: 0
-//   },
-//   { type: 'deposit', amount: 50000, balance: 50000, currentBalance: 0 }
-// ]
-// // let data
-// // for( element in userHistories){
-//    // if(element.type === "deposit"){
-//        // return data.push(element) }
-//   // console.log(userHistories)
+  const userHistories =[
+  { type: 'deposit', amount: 2000, balance: 6000, currentBalance: 0 },
+  {
+    type: 'deposit',
+    amount: 100000,
+    balance: 100000,
+    currentBalance: 0
+  },
+  { type: 'deposit', amount: 50000, balance: 50000, currentBalance: 0 }
+]};
+// let data
+// for( element in userHistories){
+   // if(element.type === "deposit"){
+       // return data.push(element) }
+  // console.log(userHistories)
 
 
 
-// const historyDeposit = async (user) => {
-//   const historyRawData = await fs.readFile("history.json", "utf-8");
-//   const history = JSON.parse(historyRawData);
+const historyDeposit = async (user) => {
+  const historyRawData = await fs.readFile("history.json", "utf-8");
+  const history = JSON.parse(historyRawData);
 
-//   const userHistories = history[user.username] || [];
+  const userHistories = history[user.username] || [];
 
-//   const deposit = userHistories.filter((item) => item.type === "deposit");
+  const deposit = userHistories.filter((item) => item.type === "deposit");
 
-//   if (deposit.length === 0) {
-//     console.log("Orlogiin tvvh oldsongvi");
-//     return;
-//   }
+  if (deposit.length === 0) {
+    console.log("Orlogiin tvvh oldsongvi");
+    return;
+  }
 
-//   console.log("===Deposithistory===");
-//   deposit.forEach((item, i) =>
-//     console.log(`${i + 1}. +${item.amount}₮ (${item.date})`)
-//   );
-//   console.log("historyDeposit")
-// };
+  console.log("===Deposithistory===");
+  deposit.forEach((item, i) =>
+    console.log(`${i + 1}. +${item.amount}₮ (${item.date})`)
+  );
+  console.log("historyDeposit")
+};
 
-// const historyWithdraw = async (user) => {
-//   const historyRawData = await fs.readFile("history.json", "utf-8");
-//   const history = JSON.parse(historyRawData);
+const historyWithdraw = async (user) => {
+  const historyRawData = await fs.readFile("history.json", "utf-8");
+  const history = JSON.parse(historyRawData);
 
-//   const userHistories = history[user.username] || [];
+  const userHistories = history[user.username] || [];
 
-//   const withdraw = userHistories.filter((item) => item.type === "withdraw");
+  const withdraw = userHistories.filter((item) => item.type === "withdraw");
 
-//   if (withdraw.length === 0) {
-//     console.log("Zarlagiin tvvh oldsongvi.");
-//     return;
-//   }
+  if (withdraw.length === 0) {
+    console.log("Zarlagiin tvvh oldsongvi.");
+    return;
+  }
 
-//   console.log("===Withdrawhistory===");
-//   withdraw.forEach((item, i) =>
-//     console.log(`${i + 1}. -${item.amount}₮ (${item.date})`)
-//   );
-//   console.log("historyWithdraw")
-// };
+  console.log("===Withdrawhistory===");
+  withdraw.forEach((item, i) =>
+    console.log(`${i + 1}. -${item.amount}₮ (${item.date})`)
+  );
+  console.log("historyWithdraw")
+};
--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ( bankRouter ) from "./routers/bank.js";
 import { userRouter } from "./routers/user.js";
+import cors from "cors";
 
 const app = express();
 
@@ -9,8 +10,9 @@ app.use(express.json());
 
 app.use(bankRouter);
 app.use(userRouter);
-
+app.use(cors());
 
 app.listen(3000, () => {
-  console.log("3000");
+  console.log("3000")
 });
+
